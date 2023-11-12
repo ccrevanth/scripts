499e0c4fd7f6c69e36bf9777cda95671967380d7
crevanth <crevanthh@gmail.com>
2023-11-12 04:42:57 +0000

    scripts: Update setup_environment

--- setup_environment.sh
+++ setup_environment.sh
@@ -31,30 +31,44 @@ sudo apt-get update || display_error "Failed to update package list."
 sudo apt-get install -y openjdk-8-jdk || display_error "Failed to install OpenJDK 8."
 
 # Install required packages
-required_packages="bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev"
+required_packages="bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev"
 sudo apt-get install -y $required_packages || display_error "Failed to install required packages."
 
 # Set up git
-git config --global user.email "crevanthh@gmail.com" || display_error "Failed to configure git user email."
-git config --global user.name "crevanth" || display_error "Failed to configure git username."
+if ! git config --global user.email "crevanthh@gmail.com"; then
+  display_error "Failed to configure git user email."
+fi
 
-# Ask whether to configure ccache
-if prompt_yes_no "Do you want to configure ccache?"; then
-  # Download and set up repo
-  mkdir -p ~/bin || display_error "Failed to create ~/bin directory."
-  curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo || display_error "Failed to download 'repo'."
-  chmod a+x ~/bin/repo || display_error "Failed to make 'repo' executable."
-  echo 'export PATH=~/bin:$PATH' >> ~/.bashrc || display_error "Failed to add 'repo' to PATH."
-  source ~/.bashrc || display_error "Failed to source ~/.bashrc."
+if ! git config --global user.name "crevanth"; then
+  display_error "Failed to configure git username."
+fi
 
-  # Configure ccache
-  echo 'export USE_CCACHE=1' >> ~/.bashrc || display_error "Failed to configure USE_CCACHE."
-  echo 'export CCACHE_EXEC=/usr/bin/ccache' >> ~/.bashrc || display_error "Failed to configure CCACHE_EXEC."
-  ccache -M 150G || display_error "Failed to set ccache size."
-  echo 'export CCACHE_COMPRESS=1' >> ~/.bashrc || display_error "Failed to configure CCACHE_COMPRESS."
-  source ~/.bashrc || display_error "Failed to source ~/.bashrc."
+# Install Git LFS
+git lfs install || display_error "Failed to install Git LFS."
+
+# Check if ccache is already configured
+if ! is_ccache_configured; then
+  # Ask whether to configure ccache
+  if prompt_yes_no "Do you want to configure ccache?"; then
+    # Download and set up repo
+    mkdir -p ~/bin || display_error "Failed to create ~/bin directory."
+    curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo || display_error "Failed to download 'repo'."
+    chmod a+x ~/bin/repo || display_error "Failed to make 'repo' executable."
+    echo 'export PATH=~/bin:$PATH' >> ~/.bashrc || display_error "Failed to add 'repo' to PATH."
+    source ~/.bashrc || display_error "Failed to source ~/.bashrc."
+
+    # Configure ccache with 50GB size and compression enabled
+    echo 'export USE_CCACHE=1' >> ~/.bashrc || display_error "Failed to configure USE_CCACHE."
+    echo 'export CCACHE_EXEC=/usr/bin/ccache' >> ~/.bashrc || display_error "Failed to configure CCACHE_EXEC."
+    ccache -M 50G || display_error "Failed to set ccache size to 50GB."
+    echo 'export CCACHE_COMPRESS=1' >> ~/.bashrc || display_error "Failed to configure CCACHE_COMPRESS."
+    ccache -o compression=true || display_error "Failed to enable ccache compression."
+    source ~/.bashrc || display_error "Failed to source ~/.bashrc."
+  else
+    echo "Skipping ccache configuration."
+  fi
 else
-  echo "Skipping ccache configuration."
+  echo "ccache is already configured. Skipping ccache configuration."
 fi
 
 echo "Development environment setup complete."
